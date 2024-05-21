0b654c5dbfde58a5906014d1a98c9ef67f86f911
Magali Courté <courte.magali@gmail.com>
2024-05-21 17:06:08 +0200

    Màj view.py

--- SoftDesk_Support/authentication/views.py
+++ SoftDesk_Support/authentication/views.py
@@ -1,13 +1,52 @@
 from rest_framework import viewsets
-# from .models import CustomUser
+from .models import CustomUser
+from .serializers import (
+    CustomUserListSerializer,
+    CustomUserUpdateSerializer,
+    CustomUserCreateSerializer,
+    CustomUserDetailSerializer
+)
+from .permissions import (
+    IsAdmin,
+    IsAdminAuthenticated,
+    AllowAnonymousAccess
+)
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
-    """
-    Permet de gérer les opérations CRUD sur le modèle CustomUser.
+    queryset = CustomUser.objects.all()
 
-    Create : Créer un User.
-    Read : Visualiser un User.
-    Update : Modifier un User.
-    Delete : Supprimer un User.
-    """
+    serializer_mapping = {
+        "list": CustomUserListSerializer,
+        "retrieve": CustomUserDetailSerializer,
+        "create": CustomUserCreateSerializer,
+        "update": CustomUserUpdateSerializer,
+        "partial_update": CustomUserUpdateSerializer,
+    }
+
+    def get_serializer_class(self):
+        """
+        Retourne la liste des permissions nécessaires pour chaque action de la vue.
+        """
+        return self.serializer_mapping.get(self.action, self.serializer_class)
+
+    def get_permissions(self):
+
+        if self.action == "retrieve":
+            permission_classes = [IsAdmin]
+        elif self.action in [
+            "list",
+            "update",
+            "partial_update",
+            "destroy",
+        ]:
+            permission_classes = [IsAdminAuthenticated]
+        elif self.action == "create":
+            permission_classes = [AllowAnonymousAccess]
+        else:
+            permission_classes = []
+
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        return CustomUser.objects.all().order_by("id")
