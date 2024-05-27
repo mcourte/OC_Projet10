9da5974d272ba072df853b4e91b4296e36584a70
Magali Courté <courte.magali@gmail.com>
2024-05-27 16:20:19 +0200

    Maj signals.py

--- SoftDesk_Support/project/signals.py
+++ SoftDesk_Support/project/signals.py
@@ -1,11 +1,10 @@
-# SoftDesk_Support/signals.py
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from .models import Project, Issue, Comment
 
 User = get_user_model()
-
 
 @receiver(pre_delete, sender=User)
 def reassign_user_projects_issues_comments(sender, instance, **kwargs):
@@ -14,19 +13,19 @@ def reassign_user_projects_issues_comments(sender, instance, **kwargs):
         raise ValueError("Aucun utilisateur administrateur trouvé pour réattribuer les projets.")
 
     # Réaffecter les projets
-    projects = Project.objects.filter(author=instance)
+    projects = Project.objects.filter(contributor_owner=instance)
     for project in projects:
-        project.author = admin_user
+        project.contributor_owner = admin_user
         project.save()
 
     # Réaffecter les issues
     issues = Issue.objects.filter(author=instance)
     for issue in issues:
-        issue.author = issue.project.author
+        issue.author = issue.project.contributor_owner
         issue.save()
 
     # Réaffecter les commentaires
     comments = Comment.objects.filter(author=instance)
     for comment in comments:
-        comment.author = comment.issue.project.author
+        comment.author = comment.issue.project.contributor_owner
         comment.save()
