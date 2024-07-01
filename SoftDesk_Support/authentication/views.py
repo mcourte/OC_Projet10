f0dffacbbfbb2fddeaf4bc742fdf6dbbdf4b92d4
Magali Courté <courte.magali@gmail.com>
2024-07-01 22:48:42 +0200

    Green code

--- SoftDesk_Support/authentication/views.py
+++ SoftDesk_Support/authentication/views.py
@@ -1,9 +1,5 @@
 from rest_framework import permissions, viewsets, status
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.contrib.auth import authenticate
 from django.shortcuts import redirect
-from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import CustomUser
@@ -26,19 +22,6 @@ class RootView(APIView):
         if not request.user.is_authenticated:
             return redirect('login')
         return redirect('projects')
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class CustomLoginView(APIView):
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
