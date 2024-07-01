9cc819ddebd5a2216144b839eff407906dc16c21
Magali Courté <courte.magali@gmail.com>
2024-07-01 17:46:38 +0200

    Green code

--- SoftDesk_Support/project/views.py
+++ SoftDesk_Support/project/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.exceptions import PermissionDenied
 from .models import Project, Issue, Comment, Contributor
 from .serializers import (
     ProjectSerializer,
@@ -19,7 +19,6 @@ class ProjectListViewSet(viewsets.ModelViewSet):
     """
     Permet de voir la liste des Projets existants et d'en créer un nouveau.
     """
-
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
@@ -28,14 +27,15 @@ class ProjectListViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'list':
             return ProjectListSerializer
-        if self.action in ['create', 'update', 'partial_update']:
+        elif self.action in ['create', 'update', 'partial_update']:
             return ProjectCreateUpdateSerializer
+        return ProjectSerializer  # Fallback serializer
 
     def create(self, request, *args, **kwargs):
-        serializer = ProjectCreateUpdateSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save(author=request.user)
-            return redirect('projects')
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -53,100 +53,75 @@ class ProjectDetailViewSet(viewsets.ModelViewSet):
     lookup_field = 'project_id'
 
     def get_queryset(self):
-        return Project.objects.all()
+        return self.queryset
 
-    def post(self, request, *args, **kwargs):
-        action = request.data.get('action')
+    def create(self, request, *args, **kwargs):
+        serializer = ProjectCreateUpdateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response({"message": "Le projet a été créé avec succès."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if action == 'update':
-            instance = self.get_object()
-            if request.user == instance.author:
-                serializer = ProjectCreateUpdateSerializer
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(
-                        {"message": "Le projet a été modifié avec succès."},
-                        status=status.HTTP_200_OK
-                    )
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                raise ValidationError("Seul l'auteur du Projet peut le modifier.")
-
-        elif action == 'destroy':
-            instance = self.get_object()
-            if request.user == instance.author:
-                instance.delete()
-                return redirect('projects')
-            else:
-                raise ValidationError("Seul l'auteur du Projet peut le supprimer.")
-
-        else:
-            raise ValidationError("Action non valide.")
-
-
-class ContributorViewSet(viewsets.ModelViewSet):
-    serializer_class = ContributorSerializer
-    permission_classes = [IsAuthor | IsContributor | IsAdmin]
-    lookup_field = 'contributor_id'
-
-    def get_project(self):
-        project_id = self.kwargs['project_id']
-        return get_object_or_404(Project, project_id=project_id)
-
-    def get_queryset(self):
-        project_id = self.kwargs.get('project_id')
-        return Contributor.objects.filter(project__project_id=project_id)
-
-    def perform_create(self, serializer):
-        project = self.get_project()
-        serializer.save(project=project)
-
-    def post(self, request, *args, **kwargs):
-        action = request.data.get('action', 'create')
-        project = self.get_project()
-
-        if action == 'create':
-            if not request.user.is_authenticated:
-                return Response({"detail": "Vous devez être connecté pour effectuer cette action."},
-                                status=status.HTTP_401_UNAUTHORIZED)
-
-            if request.user != project.author:
-                return Response({"detail": "Seul l'auteur du projet peut ajouter des contributeurs."},
-                                status=status.HTTP_403_FORBIDDEN)
-
-            # Ajoutez 'project' aux données envoyées
-            request.data['project'] = project.id
-
-            serializer = self.get_serializer(data=request.data)
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.author:
+            serializer = ProjectCreateUpdateSerializer(instance, data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response({"message": "Le projet a été modifié avec succès."}, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"detail": "Action non prise en charge."},
-                        status=status.HTTP_400_BAD_REQUEST)
+        raise PermissionDenied("Seul l'auteur du Projet peut le modifier.")
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user == instance.author:
             instance.delete()
-            # Redirection vers une autre URL après suppression
-            return redirect('projects')
-        else:
-            return Response(
-                {"message": "Seul l'auteur du Projet peut le supprimer."},
-                status=status.HTTP_403_FORBIDDEN
-            )
+            return Response({"message": "Le projet a été supprimé avec succès."}, status=status.HTTP_204_NO_CONTENT)
+        raise PermissionDenied("Seul l'auteur du Projet peut le supprimer.")
+
+
+class ContributorViewSet(viewsets.ModelViewSet):
+    """
+    Gère les contributeurs pour un projet spécifique.
+    """
+    serializer_class = ContributorSerializer
+    permission_classes = [IsAuthor | IsContributor | IsAdmin]
+    lookup_field = 'contributor_id'
+
+    def get_queryset(self):
+        project_id = self.kwargs.get('project_id')
+        return Contributor.objects.filter(project_id=project_id)
+
+    def perform_create(self, serializer):
+        project_id = self.kwargs.get('project_id')
+        project = get_object_or_404(Project, project_id=project_id)
+        serializer.save(project=project)
+
+    def create(self, request, *args, **kwargs):
+        project_id = self.kwargs.get('project_id')
+        project = get_object_or_404(Project, project_id=project_id)
+
+        if request.user != project.author:
+            raise PermissionDenied("Seul l'auteur du projet peut ajouter des contributeurs.")
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(project=project)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.project.author:
+            instance.delete()
+            return Response({"message": "Le contributeur a été supprimé avec succès."},
+                            status=status.HTTP_204_NO_CONTENT)
+        raise PermissionDenied("Seul l'auteur du projet peut supprimer des contributeurs.")
 
 
 class IssueViewSet(viewsets.ModelViewSet):
-
     """
-    Permet de gérer les opérations CRUD sur le modèle Issue.
-    Create : Créer une Issue.
-    Read : Visualiser une Issue.
-    Update : Modifier une Issue.
-    Delete : Supprimer une Issue.
+    Gère les opérations CRUD sur le modèle Issue.
     """
     serializer_class = IssueSerializer
     permission_classes = [IsAuthor | IsContributor]
@@ -154,97 +129,72 @@ class IssueViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         project_id = self.kwargs.get('project_id')
-        if self.action == 'list':
-            # Vérifie si l'utilisateur est un contributeur du projet
-            project = get_object_or_404(Project, project_id=project_id)
-            if project.contributors.filter(pk=self.request.user.pk).exists():
-                return Issue.objects.filter(project_id=project_id)
-            else:
-                raise PermissionDenied("Vous n'êtes pas autorisé à voir les issues de ce projet.")
-        return Issue.objects.all()
+        return Issue.objects.filter(project_id=project_id)
 
-    def perform_create(self, serializer):
-        project_id = self.request.data.get('project_id')
+    def create(self, request, *args, **kwargs):
+        project_id = self.kwargs.get('project_id')
         project = get_object_or_404(Project, project_id=project_id)
-        serializer.save(author=self.request.user, project=project)
 
-    def post(self, request, *args, **kwargs):
-        action = request.data.get('action', 'create')
-        if action == 'update':
-            instance = self.get_object()
-            if request.user == instance.author:
-                serializer = self.get_serializer(instance, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(
-                        {"message": "L'Issue a été modifiée avec succès."},
-                        status=status.HTTP_200_OK
-                    )
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                raise ValidationError("Seul l'auteur de l'issue peut la modifier.")
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user, project=project)
+            return Response({"message": "L'Issue a été créée avec succès."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        elif action == 'destroy':
-            instance = self.get_object()
-            if request.user == instance.author:
-                instance.delete()
-                return Response(
-                    {"message": "L'Issue a été supprimée avec succès."},
-                    status=status.HTTP_204_NO_CONTENT,
-                )
-            else:
-                raise ValidationError("Seul l'auteur de l'issue peut la supprimer.")
-        else:
-            raise ValidationError("Action non valide.")
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.author:
+            serializer = self.get_serializer(instance, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "L'Issue a été modifiée avec succès."}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        raise PermissionDenied("Seul l'auteur de l'issue peut la modifier.")
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.author:
+            instance.delete()
+            return Response({"message": "L'Issue a été supprimée avec succès."}, status=status.HTTP_204_NO_CONTENT)
+        raise PermissionDenied("Seul l'auteur de l'issue peut la supprimer.")
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Gère les opérations CRUD sur le modèle Comment.
+    """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     lookup_field = 'comment_id'
 
     def get_queryset(self):
         issue_id = self.kwargs.get('issue_id')
-        if issue_id:
-            return self.queryset.filter(issue_id=issue_id)
-        return self.queryset.none()
+        return self.queryset.filter(issue_id=issue_id)
 
     def create(self, request, *args, **kwargs):
-        issue_id = kwargs.get('issue_id')  # Utilisez kwargs pour récupérer issue_id
-
-        # Vérifiez si l'issue existe
+        issue_id = self.kwargs.get('issue_id')
         issue = get_object_or_404(Issue, issue_id=issue_id)
 
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save(author=request.user, issue=issue)
-            return Response(
-                {"message": "Le commentaire a été créé avec succès."},
-                status=status.HTTP_201_CREATED
-            )
+            return Response({"message": "Le commentaire a été créé avec succès."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user == instance.author:
-            serializer = CommentSerializer(instance, data=request.data, partial=True)
+            serializer = self.get_serializer(instance, data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(
-                    {"message": "Le commentaire a été modifié avec succès."},
-                    status=status.HTTP_200_OK
-                )
+                return Response({"message": "Le commentaire a été modifié avec succès."}, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            raise ValidationError("Seul l'auteur du Comment peut le modifier.")
+        raise PermissionDenied("Seul l'auteur du commentaire peut le modifier.")
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user == instance.author:
             instance.delete()
-            return Response(
-                {"message": "Le Comment a été supprimé avec succès."},
-                status=status.HTTP_204_NO_CONTENT,
-            )
-        else:
-            raise ValidationError("Seul l'auteur du Comment peut le supprimer.")
+            return Response({"message": "Le commentaire a été supprimé avec succès."},
+                            status=status.HTTP_204_NO_CONTENT)
+        raise PermissionDenied("Seul l'auteur du commentaire peut le supprimer.")
